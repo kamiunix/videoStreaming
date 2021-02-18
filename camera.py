c09a937b389e9276002927d24750f47217d37bcc
samuel <samuel@avestec.com>
2021-02-18 13:31:14 -0800

    working demonstration

--- camera.py
+++ camera.py
@@ -1,22 +1,16 @@
 #camera.py
 # import the necessary packages
 import cv2
-# defining face detector
-face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
-ds_factor=0.6
+
 class VideoCamera(object):
     def __init__(self):
-       #capturing video
-       self.video = cv2.VideoCapture(0)
-    
+        self.video = cv2.VideoCapture(0)
+
+
     def __del__(self):
-        #releasing camera
         self.video.release()
 
-def get_frame(self):
-       #extracting frames
-        ret, frame = self.video.read()
-        frame=cv2.resize(frame,None,fx=ds_factor,fy=ds_factor,
-        # encode OpenCV raw frame to jpg and displaying it
-        ret, jpeg = cv2.imencode('.jpg', frame)
+    def get_frame(self):
+        success, image = self.video.read()
+        ret, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
